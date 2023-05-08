cee623a25026f5d26297016115b1bdd927f41152
ssonggary <84754325+ssonggary@users.noreply.github.com>
2023-05-08 13:55:58 +0900

    2023-05-08
    svg 넣어보기

---
+++ src/main_svg.js
@@ -0,0 +1,57 @@
+//윈도우(브라우저)의 크기를 변수에 담는다.
+const windowHeight = window.innerHeight;
+const windowWidth = window.innerWidth;
+
+// const div = document.getElementById('areaToRender');
+
+// module aliases
+let Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Bodies = Matter.Bodies,
+  Composite = Matter.Composite,
+  Mouse = Matter.Mouse,
+  MouseConstraint = Matter.MouseConstraint;
+
+// create an engine
+let engine = Engine.create();
+
+// create a renderer
+let render = Render.create({
+  element: document.getElementById("areaToRender"),
+  engine: engine,
+  options: {
+    background: "#fafafa",
+    wireframes: false,
+  },
+});
+
+// create two boxes and a ground
+let boxA = Bodies.rectangle(400, 200, 80, 80);
+let boxB = Bodies.rectangle(450, 50, 80, 80);
+let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+let runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
+
+let mouse = Mouse.create(render.canvas);
+let mouseConstraint = MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    stiffness: 0.2,
+    render: {
+      visible: false,
+    },
+  },
+});
+
+Composite.add(engine.world, mouseConstraint);
