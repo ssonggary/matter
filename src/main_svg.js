208d6a71108b7ec4302685058864a6e1db7fbee3
ssonggary <84754325+ssonggary@users.noreply.github.com>
2023-05-08 17:58:14 +0900

    2023-05-08
    이미지 넣기 작업

--- src/main_svg.js
+++ src/main_svg.js
@@ -4,6 +4,7 @@ const windowWidth = window.innerWidth;
 const container = document.getElementById("areaToRender");
 const THICCNESS = 60;
 
+const SVG_SELECTOR = "#svg";
 const SVG_WIDTH_IN_PX = 100;
 const SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH = 0.3;
 
@@ -38,7 +39,7 @@ let render = Render.create({
     height: container.clientHeight,
     background: "#fafafa",
     wireframes: false,
-    // showAngleIndicator: true,
+    showAngleIndicator: false,
   },
 });
 
@@ -64,8 +65,8 @@ function handleResize(container) {
 window.addEventListener("resize", () => handleResize(container));
 
 // create two boxes and a ground
-let boxA = Bodies.rectangle(400, 200, 80, 80);
-let circleA = Bodies.circle(500, 500, 75, {
+let boxA = Bodies.rectangle(400, 0, 80, 80);
+let circleA = Bodies.circle(500, 10, 75, {
   restitution: 0.5,
   render: {
     sprite: {
@@ -76,9 +77,9 @@ let circleA = Bodies.circle(500, 500, 75, {
   },
 });
 createCircle();
-createSvgBodies();
+// createSvgBodies();
 
-let boxB = Bodies.rectangle(450, 50, 80, 80);
+let boxB = Bodies.rectangle(450, 10, 80, 80);
 let ground = Bodies.rectangle(container.clientWidth / 2, container.clientHeight + THICCNESS / 2, 27184, THICCNESS, { isStatic: true });
 let leftWall = Bodies.rectangle(0 - THICCNESS / 2, container.clientHeight / 2, THICCNESS, container.clientHeight * 5, { isStatic: true });
 let rightWall = Bodies.rectangle(container.clientWidth + THICCNESS / 2, container.clientHeight / 2, THICCNESS, container.clientHeight * 5, { isStatic: true });
@@ -110,8 +111,16 @@ function createCircle() {
       strokeStyle: "#ECA869",
     },
   });
+  Composite.add(engine.world, circle);
 }
 
-function createSvgBodies() {
-  const paths = document.querySelectorAll;
-}
+// function createSvgBodies() {
+//   const paths = document.querySelectorAll(SVG_SELECTOR);
+//   paths.forEach((path, index) => {
+//     let vertices = Svg.pathToVertices(path);
+//     let scaleFactor = (container.clientWidth * SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH) / SVG_WIDTH_IN_PX;
+//     vertices = Vectices.scale(vertices, scaleFactor, scaleFactor);
+//     let svgBody = Bodies.fromVertices(index * SVG_WIDTH_IN_PX + 200, 0, [vertices]);
+//   });
+//   Composite.add(engine.world, svgBody);
+// }
